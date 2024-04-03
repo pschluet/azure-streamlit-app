4a039b22e05519632257ee8ff9c5dec5466960c4
Paul Schlueter <paul@paulschlueter.com>
2024-04-02 20:10:59 -0500

    Create Linux Web App

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.97.1"
     }
   }
 
@@ -26,4 +26,24 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_resource_group" "rg" {
   name     = "azure-streamlit-app"
   location = "eastus"
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = "azure-streamlit-app-sp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "lwa" {
+  name                = "azure-streamlit-app-lwa"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.sp.location
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  site_config {
+    always_on         = false
+    use_32_bit_worker = true
+  }
 }
